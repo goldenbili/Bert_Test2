0f44372c219359da0e905424dd39feffbfb11636
partydog <waeechang@gmail.com>
2019-05-16 17:10:48 +0800

    Update run_squad.py

--- run_squad.py
+++ run_squad.py
@@ -1495,7 +1495,7 @@ def set_eval_examples(questions,documents):
 
 def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
-  print("willy test on 201905161600")
+  print("willy test on 201905161710")
   
   bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
 
