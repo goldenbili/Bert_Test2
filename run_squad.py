d83de2388e0451d04e0eb859eed7c097a3579c54
partydog <waeechang@gmail.com>
2019-05-21 15:22:36 +0800

    Update run_squad.py

--- run_squad.py
+++ run_squad.py
@@ -1099,10 +1099,10 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
     all_predictsInOneQues.append(
         _AllPredictResultsInOneQuestion(doc_text=example.doc_tokens,PredictListOneDoc=all_predictsInOneDoc))
         if predict_result_index == 1:
-        print ("all_predictsInOneQues")
-        print(all_predictsInOneQues)
-        print('-'*15)
-        print('\n')
+            print ("all_predictsInOneQues")
+            print(all_predictsInOneQues)
+            print('-'*15)
+            print('\n')
  
     #----------------------------------------------
     # presupposition : Question is in order
