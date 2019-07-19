50a75c022b19bee02c0cd1e0c71cc342aebd2e62
partydog <waeechang@gmail.com>
2019-07-19 09:58:12 +0800

    Update run_squad.py

--- run_squad.py
+++ run_squad.py
@@ -47,7 +47,7 @@ getcontext().prec = 50
 #Willy Define
 example_in_set_eval_examples = 0
 example_in_write_predictions = 0
-predict_result_index = 0
+predict_result_index = 1
 checkState_in_AtenResult = 0
 checkState_in_GetAnswer = 0
 willy_check_code = "willy test on 201907101548"
