451c8222ada456f2a6866233374eee665b8f886a
partydog <waeechang@gmail.com>
2019-07-23 17:00:44 +0800

    Update run_squad_coding.py

--- run_squad_coding.py
+++ run_squad_coding.py
@@ -1212,9 +1212,8 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
   for i, entry_predicts in enumerate(all_predicts):
     tp_ques = entry_predicts.question   
     
-    doc_names=[], doc_scores = np.empty(0)
-    if ranker:
-        doc_names, doc_scores = ranker.closest_docs(tp_ques, FLAGS.retriever_ranker)  
+    #doc_names=[], doc_scores = np.empty(0)
+    doc_names, doc_scores = ranker.closest_docs(tp_ques, FLAGS.retriever_ranker)  
         
         
     tp_no_answer = entry_predicts.no_answer
