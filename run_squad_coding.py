179f48ecb960a176a2f629d2a3bdabb6d11b5611
partydog <waeechang@gmail.com>
2019-07-25 13:06:27 +0800

    Update run_squad_coding.py

--- run_squad_coding.py
+++ run_squad_coding.py
@@ -1677,7 +1677,7 @@ def read_squad_question(input_file):
                     questions.append(qa["question"])
     return questions
 
-def set_eval_examples(questions):
+def set_eval_examples(questions,DOC2IDX):
     def is_whitespace(c):
         if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
             return True
@@ -1871,11 +1871,9 @@ def main(_):
         print('Show DOCIDX')
         print(DOC2IDX)
 
-        '''
-        print('ID:1 ,doc:%s' %(DOC2IDX[1]))
+
         for i, DOCID in enumerate(DOC2IDX) :
             print('ID:%d ,doc:%s' %(i,DOCID))
-        '''
         #------------------------------------------------------
         
     else:
@@ -1903,7 +1901,7 @@ def main(_):
     # ---------------------------------------------------
 
     #print('WillyTest(2)...do Set eval_examples')
-    eval_examples=set_eval_examples(questions)
+    eval_examples=set_eval_examples(questions,DOC2IDX)
 
     #print('WillyTest(2.1)...do FeatureWriter')
     eval_writer = FeatureWriter(
