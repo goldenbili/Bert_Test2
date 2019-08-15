fd745f2748466541e9613c001affe5e789b633b1
partydog <waeechang@gmail.com>
2019-08-15 18:50:17 +0800

    Update run_squad_temp_findDoc_findAns.py

--- run_squad_temp_findDoc_findAns.py
+++ run_squad_temp_findDoc_findAns.py
@@ -49,7 +49,7 @@ getcontext().prec = 50
 #Willy Define
 example_in_set_eval_examples = 0
 example_in_write_predictions = 0
-predict_result_index = 1
+predict_result_index = 0
 checkState_in_AtenResult = 0
 checkState_in_AtenResult2 = 0
 checkState_in_GetAnswer = 0
