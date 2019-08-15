0ce9e4ba5ef4f47a8d35e569cbb1631be2488e6c
partydog <waeechang@gmail.com>
2019-08-15 13:16:50 +0800

    Update run_squad_coding_20190812.py

--- run_squad_coding_20190812.py
+++ run_squad_coding_20190812.py
@@ -1156,19 +1156,20 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
     # save answer dataset
     #----------------------------------------------
     all_predictsInOneDoc = [] 
+    print('go to (1)')
     for i, entry in enumerate(nbest):        
         if i==2:
-            #print('In state 2')
+            print('In state 2')
             break
         tp_answer = entry.text    
         if i==0 :
             if tp_answer.isspace() or not tp_answer:
-                #print('In state 0,tp_ans: %s' %tp_answer)
+                print('In state 0,tp_ans: %s' %tp_answer)
                 continue
         if i == 1 and len(all_predictsInOneDoc)!=0:
-            #print('In state 1,tp_ans: %s' %tp_answer)
+            print('In state 1,tp_ans: %s' %tp_answer)
             break
-        #print('In state set pridict. tp_ans: %s' %tp_answer )    
+        print('In state set pridict. tp_ans: %s' %tp_answer )    
         all_predictsInOneDoc.append(
             _AllPredictResultsInOneDocument(
                 answer=entry.text, 
@@ -1177,6 +1178,7 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
                 end = entry.end_logit
             )
         )
+    print('go to (2)')  
     #----------------------------------------------
     # End of save answer dataset
     if predict_result_index == 1:
@@ -1190,6 +1192,7 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
         print('-'*15)
         print('\n')
     # append predicts to OneQues
+    print('go to (3)')
     #----------------------------------------------
     tp_docscore = 0.0
     if example.doc_id in doc_names :
