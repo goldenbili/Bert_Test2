c2f6adee65b74f43d1941ed063a33b71fdeb1312
partydog <waeechang@gmail.com>
2019-08-15 14:18:42 +0800

    Update run_squad_coding_20190812.py

--- run_squad_coding_20190812.py
+++ run_squad_coding_20190812.py
@@ -49,7 +49,7 @@ getcontext().prec = 50
 #Willy Define
 example_in_set_eval_examples = 0
 example_in_write_predictions = 0
-predict_result_index = 1
+predict_result_index = 0
 checkState_in_AtenResult = 0
 checkState_in_AtenResult2 = 0
 checkState_in_GetAnswer = 0
@@ -1156,20 +1156,20 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
     # save answer dataset
     #----------------------------------------------
     all_predictsInOneDoc = [] 
-    print('go to (1)')
+    #print('go to (1)')
     for i, entry in enumerate(nbest):        
         if i==2:
-            print('In state 2')
+            #print('In state 2')
             break
         tp_answer = entry.text    
         if i==0 :
             if tp_answer.isspace() or not tp_answer:
-                print('In state 0,tp_ans: %s' %tp_answer)
+                #print('In state 0,tp_ans: %s' %tp_answer)
                 continue
         if i == 1 and len(all_predictsInOneDoc)!=0:
-            print('In state 1,tp_ans: %s' %tp_answer)
+            #print('In state 1,tp_ans: %s' %tp_answer)
             break
-        print('In state set pridict. tp_ans: %s' %tp_answer )    
+        #print('In state set pridict. tp_ans: %s' %tp_answer )    
         all_predictsInOneDoc.append(
             _AllPredictResultsInOneDocument(
                 answer=entry.text, 
@@ -1182,7 +1182,7 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
     
     
     
-    print('go to (2)')  
+    #print('go to (2)')  
     #----------------------------------------------
     # End of save answer dataset
     if predict_result_index == 1:
@@ -1196,7 +1196,7 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
         print('-'*15)
         print('\n')
     # append predicts to OneQues
-    print('go to (3)')
+    #print('go to (3)')
     #----------------------------------------------
     tp_docscore = 0.0
     if example.doc_id in doc_names :
@@ -1204,9 +1204,9 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
         tp_docscore = doc_scores [tp_docindex]
         print('go to (4)')
     
-    print('go to (5)')    
-    print('all_predictsInOneQues-in set')
-    print(all_predictsInOneQues)    
+    #print('go to (5)')    
+    #print('all_predictsInOneQues-in set')
+    #print(all_predictsInOneQues)    
     all_predictsInOneQues.append(
         _AllPredictResultsInOneQuestion(
             doc_text=example.doc_tokens,
@@ -1215,7 +1215,7 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
             PredictListOneDoc=all_predictsInOneDoc
         )
     )
-    print('go to (6)')
+    #print('go to (6)')
     
     print('all_predictsInOneQues-in set')
     print(all_predictsInOneQues)
