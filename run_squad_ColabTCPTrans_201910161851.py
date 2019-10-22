1fd6793cfd1bd77dd95c203a632d55155b64b2db
partydog <waeechang@gmail.com>
2019-10-22 18:42:11 +0800

    Update run_squad_ColabTCPTrans_201910161851.py

--- run_squad_ColabTCPTrans_201910161851.py
+++ run_squad_ColabTCPTrans_201910161851.py
@@ -1413,7 +1413,7 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
             fin_Score = Score2
             choice_value = 1
             print("Answer2 State3")
-        else
+        else:
             print("Answer1 State1")
 
     ans_list.append(fin_ans)
